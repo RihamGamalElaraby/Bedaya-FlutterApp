128f113eae5983955a43c2b046ba3d2bd00ea397
bisho <girgisbishoy6@gmail.com>
2023-08-04 04:14:53 +0300

    finish labs children

--- lib/screens/pharmacy/PharmacyScreen.dart
+++ lib/screens/pharmacy/PharmacyScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 class PharmacyScreen extends StatefulWidget {
   static const String screenRoute = 'PharmacyScreen';
   const PharmacyScreen({Key? key}) : super(key: key);
@@ -8,8 +10,68 @@ class PharmacyScreen extends StatefulWidget {
 }
 
 class _PharmacyScreenState extends State<PharmacyScreen> {
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+        '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+//barcode scanner flutter ant
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        alignment: Alignment.center,
+        child: Flex(
+            direction: Axis.vertical,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                  onPressed: () => scanBarcodeNormal(),
+                  child: const Text('Barcode scan')),
+              ElevatedButton(
+                  onPressed: () => scanQR(),
+                  child: const Text('QR scan')),
+              ElevatedButton(
+                  onPressed: () => startBarcodeScanStream(),
+                  child: const Text('Barcode scan stream')),
+              Text('Scan result : $_scanBarcode\n',
+                  style: const TextStyle(fontSize: 20))
+            ]));
   }
 }
